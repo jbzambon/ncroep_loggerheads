7e65e3199d64e2afd27855277a00018267efc87e
Joseph B. Zambon <jbzambon@ncsu.edu>
2019-10-30 16:27:45 -0400

    Update ncroep.py
    added displaying values for selected data point

--- ncroep.py
+++ ncroep.py
@@ -56,8 +56,8 @@ date_start = datetime.datetime.strptime(date_start,"%Y-%m-%d")
 # For inline plotting in Jupyter Notebook
 #get_ipython().run_line_magic('pylab', 'inline')
 
-parallels = np.arange(0.,90,1.)
-meridians = np.arange(180.,360.,1.)
+parallels = np.arange(0.,90,2.)
+meridians = np.arange(180.,360.,2.)
 
 # For inline plotting in Jupyter Notebook
 #figsize(22,20)
@@ -70,16 +70,28 @@ for t in range(0,1):
     time=np.array(modis_sst_dataset['time'])
     t_ind = np.where(time==datetime.datetime.strftime((date_start + datetime.timedelta(t)),"%Y-%m-%d"+"T00:00:00Z"))
     t_ind = t_ind[0]
+    if t_ind.size == 0:
+        print('No matching time in dataset found.')
+        exit(1)
     curr_date = date_start + datetime.timedelta(t)
     # Assume coordinates are congruent among datasets, just use modis_sst
-    lat = modis_sst_dataset['lat'][:]
-    lon = modis_sst_dataset['lon'][:]
+    lon = np.array(modis_sst_dataset['lon'][:])
+    lat = np.array(modis_sst_dataset['lat'][:])
+    # Locate grid point closest to prescribed lat/lon
+    diff_lon = lon - plot_loc[0]
+    diff_lat = lat - plot_loc[1]
+    min_lon = np.argmin(abs(diff_lon))
+    min_lat = np.argmin(abs(diff_lat))
     # OPeNDAP linked datasets
     raw_sst = modis_sst_dataset['sst']
     raw_sst = raw_sst['sst'][int(t_ind),:,:]
     raw_sst = np.squeeze(raw_sst)
     raw_sst = np.ma.filled(raw_sst.astype(float), np.nan)
     raw_sst[raw_sst<-5]=np.nan; raw_sst= np.ma.array(raw_sst,mask=np.isnan(raw_sst))
+    if np.isnan(raw_sst[min_lat,min_lon]) == 0:
+        raw_sst_loc = str(round(raw_sst[min_lat,min_lon],1))
+    else:
+        raw_sst_loc = 'NaN '
     # Raw Chlor-a
     # OPeNDAP linked datasets
     raw_chla = modis_chl_dataset['chlor_a']
@@ -87,6 +99,10 @@ for t in range(0,1):
     raw_chla = np.squeeze(raw_chla)
     raw_chla = np.ma.filled(raw_chla.astype(float), np.nan)
     raw_chla[raw_chla<0]=np.nan; raw_chla= np.ma.array(raw_chla,mask=np.isnan(raw_chla))
+    if np.isnan(raw_chla[min_lat,min_lon]) == 0:
+        raw_chla_loc = str(round(raw_chla[min_lat,min_lon],3))
+    else:
+        raw_chla_loc = 'NaN '
     # DINEOF SST
     # OPeNDAP linked datasets
     dineof_sst = dineof_sst_dataset['sst']
@@ -94,6 +110,10 @@ for t in range(0,1):
     dineof_sst = np.squeeze(dineof_sst)
     dineof_sst = np.ma.filled(dineof_sst.astype(float), np.nan)
     dineof_sst[dineof_sst<-5]=np.nan; dineof_sst= np.ma.array(dineof_sst,mask=np.isnan(dineof_sst))
+    if np.isnan(dineof_sst[min_lat,min_lon]) == 0:
+        dineof_sst_loc = str(round(dineof_sst[min_lat,min_lon],1))
+    else:
+        dineof_sst_loc = 'NaN '
     # Raw SST
     # OPeNDAP linked datasets
     dineof_chla = dineof_chl_dataset['chlor_a']
@@ -101,6 +121,10 @@ for t in range(0,1):
     dineof_chla = np.squeeze(dineof_chla)
     dineof_chla = np.ma.filled(dineof_chla.astype(float), np.nan)
     dineof_chla[dineof_chla<0]=np.nan; dineof_chla= np.ma.array(dineof_chla,mask=np.isnan(dineof_chla))
+    if np.isnan(dineof_chla[min_lat,min_lon]) == 0:
+        dineof_chla_loc = str(round(dineof_chla[min_lat,min_lon],3))
+    else:
+        dineof_chla_loc = 'NaN '
     plt.clf()
     plt.suptitle('1km Observed and Cloud Free: ' + curr_date.strftime("%d %b %Y %H"+"UTC"),fontsize=36,family='Helvetica')
     # Raw SST
@@ -119,6 +143,8 @@ for t in range(0,1):
     map.drawmeridians(meridians,labels=[0,0,0,1],fontsize=18)
     x_loc,y_loc = map(plot_loc[0], plot_loc[1])
     map.plot(x_loc, y_loc, 'ro', fillstyle='none',markersize=24)   #location
+    tx_loc,ty_loc = map(-79.9, 37.5)
+    plt.text(tx_loc,ty_loc,''.join([raw_sst_loc,'$^\circ$C']),fontsize=36,fontweight='bold',color='r')   #location
     plt.title(('Original SST ($^\circ$C)'),fontsize=24,family='Helvetica')
     cbar=map.colorbar(location='right',ticks=np.arange(sst_range[0],sst_range[1]+0.01,2))
     cbar.ax.tick_params(labelsize=20)
@@ -137,6 +163,8 @@ for t in range(0,1):
     map.drawmeridians(meridians,labels=[0,0,0,1],fontsize=18)
     x_loc,y_loc = map(plot_loc[0], plot_loc[1])
     map.plot(x_loc, y_loc, 'ro', fillstyle='none',markersize=24)   #location
+    tx_loc,ty_loc = map(-79.9, 37.5)
+    plt.text(tx_loc,ty_loc,''.join([dineof_sst_loc,'$^\circ$C']),fontsize=36,fontweight='bold',color='r')   #location
     plt.title(('Cloud Free SST ($^\circ$C)'),fontsize=24,family='Helvetica')
     cbar=map.colorbar(location='right',ticks=np.arange(sst_range[0],sst_range[1]+0.01,2))
     cbar.ax.tick_params(labelsize=20)
@@ -155,6 +183,8 @@ for t in range(0,1):
     map.drawmeridians(meridians,labels=[0,0,0,1],fontsize=18)
     x_loc,y_loc = map(plot_loc[0], plot_loc[1])
     map.plot(x_loc, y_loc, 'ro', fillstyle='none',markersize=24)   #location
+    tx_loc,ty_loc = map(-79.9, 37.5)
+    plt.text(tx_loc,ty_loc,''.join([raw_chla_loc,'mg/m$^3$']),fontsize=36,fontweight='bold',color='r')   #location
     plt.title(('Original Chl-a (mg/m$^3$)'),fontsize=24,family='Helvetica')
     cbar=map.colorbar(location='right',norm=LogNorm(vmin=0.01, vmax=100),                      ticks=[0.01,0.1,1,10,100])
     cbar.ax.tick_params(labelsize=20)
@@ -174,7 +204,8 @@ for t in range(0,1):
     map.drawmeridians(meridians,labels=[0,0,0,1],fontsize=18)
     x_loc,y_loc = map(plot_loc[0], plot_loc[1])
     map.plot(x_loc, y_loc, 'ro', fillstyle='none',markersize=24)   #location
-    plt.title(('Cloud Free Chl-a (mg/m$^3$)'),fontsize=24,family='Helvetica')
+    tx_loc,ty_loc = map(-79.9, 37.5)
+    plt.text(tx_loc,ty_loc,''.join([dineof_chla_loc,'mg/m$^3$']),fontsize=36,fontweight='bold',color='r')   #location    plt.title(('Cloud Free Chl-a (mg/m$^3$)'),fontsize=24,family='Helvetica')
     cbar=map.colorbar(location='right',norm=LogNorm(vmin=0.01, vmax=100),                      ticks=[0.01,0.1,1,10,100])
     cbar.ax.tick_params(labelsize=20)
 
